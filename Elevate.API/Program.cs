5bdfd8c67a9b14b70ed53982e9feedec963ae0bb
Bojan <bojan.gavreski@outlook.com>
2026-03-05 21:32:09 +0100

    wyp

--- Elevate.API/Program.cs
+++ Elevate.API/Program.cs
@@ -13,12 +13,12 @@ namespace Elevate.API
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
-            var app = builder.Build();
+            var app = builder.Build(); 
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.MapOpenApi(); 
             }
 
             app.UseHttpsRedirection();
