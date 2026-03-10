ea34c818e8c0daa0612d2a1773e166fd6810553e
Bojan <bojan.gavreski@outlook.com>
2026-03-10 09:18:55 +0100

    summary added

--- Elevate.Models/Contracts/IElevator.cs
+++ Elevate.Models/Contracts/IElevator.cs
@@ -6,10 +6,27 @@ namespace Elevate.Models.Contracts
     {
         int Id { get; }
 
+        /// <summary>
+        /// Returns the calculated cost for the elevator based on the request
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         int CalculateCost(ElevatorRequest request);
 
+        /// <summary>
+        /// Enqueues the request in the specific elevator and starts the movement if it is idle
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         Task EnqueueRequest(ElevatorRequest request, CancellationToken cancellationToken);
 
+
+        /// <summary>
+        /// Validates the request and checks if the elevator is able to handle that request
+        /// </summary>
+        /// <param name="elevatorRequest"></param>
+        /// <returns></returns>
         bool CanEnqueue(ElevatorRequest elevatorRequest);
     }
 }
--- Elevate.Models/Contracts/IElevatorManager.cs
+++ Elevate.Models/Contracts/IElevatorManager.cs
@@ -4,8 +4,21 @@ namespace Elevate.Serices.Contracts
 {
     public interface IElevatorManager
     {
+        /// <summary>
+        /// Requests appropriate elevator to handle the reques
+        /// </summary>
+        /// <param name="elevatorRequest"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         Task RequestElevator(ElevatorRequest elevatorRequest, CancellationToken cancellationToken);
 
+
+        /// <summary>
+        /// Starts the elevator loop with randomly generated requests
+        /// Voids SRP and in prod should be in separate service 
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         Task StartElevatorLoop(CancellationToken cancellationToken);
     }
 }
